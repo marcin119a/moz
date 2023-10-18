f80bfe394c05d5e17ee3c76d93bc5e5618f903c5
amso <marcin119a@gmail.com>
2023-10-18 14:49:16 +0200

    init

---
+++ prog/moz.cpp
@@ -0,0 +1,11 @@
+/* Rozwiazanie wzorcowe do zadania MOZ
+   Autor: Marcin Wierzbiński
+   Data: 18.10.2022
+   Opis: Potem dynamik z jednostronnym przesuwaniem wskaźników.
+ */
+
+
+int main(){
+
+
+}
