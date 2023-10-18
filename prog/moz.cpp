1a012fd44a2bec352ed9433a1bfe3562870d9e22
amso <marcin119a@gmail.com>
2023-10-18 15:10:18 +0200

    wzorcówka

--- prog/moz.cpp
+++ prog/moz.cpp
@@ -1,11 +1,49 @@
-/* Rozwiazanie wzorcowe do zadania MOZ
-   Autor: Marcin Wierzbiński
-   Data: 18.10.2022
-   Opis: Potem dynamik z jednostronnym przesuwaniem wskaźników.
- */
+#include <iostream>
+#include <vector>
 
+const int MOD = 1000000007;
 
-int main(){
+int main() {
+    int n, k, q;
+    std::cin >> n >> k >> q;
 
+    std::vector<int> d(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> d[i];
+    }
 
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(k + 1, 0));
+    std::vector<std::vector<int>> df(n + 1, std::vector<int>(k + 1, 0));
+    std::vector<int> wynik(n, 0);
+
+    // Inicjalizacja dp
+    for (int j = 0; j <= k; j++) {
+        dp[1][j] = 1;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j <= k; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (j > 0) dp[i][j] += df[i - 1][j - 1];
+            if (j < k) dp[i][j] += df[i - 1][j + 1];
+            dp[i][j] %= q;
+        }
+    }
+
+    // Obliczenia wyniku
+    for (int i = 0; i < n; i++) {
+        wynik[i] = 0;
+        for (int j = 0; j <= k; j++) {
+            wynik[i] += dp[d[i]][j];
+            wynik[i] %= q;
+        }
+    }
+
+    // Wyświetlenie wyniku
+    for (int i = 0; i < n; i++) {
+        std::cout << wynik[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
